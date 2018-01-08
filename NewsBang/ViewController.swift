72b96848efdcdf9010833d185e4711f8f039c22b
xwn <woshixwn@gmail.com>
2018-01-08 09:57:56 +0800

    Initial Commit

---
+++ NewsBang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsBang
+//
+//  Created by 徐炜楠 on 2018/1/8.
+//  Copyright © 2018年 徐炜楠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
